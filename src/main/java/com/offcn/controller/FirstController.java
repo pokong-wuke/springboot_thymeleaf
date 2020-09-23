6ccc330789d2889e7d8901847559bf029bd8600b
shao123 <1059366854@qq.com>
2020-09-23 20:28:32 +0800

    springboot集成thymeleaf

--- src/main/java/com/offcn/controller/FirstController.java
+++ src/main/java/com/offcn/controller/FirstController.java
@@ -59,7 +59,7 @@ public class FirstController {
         double price=123456.789D;
         model.addAttribute("price",price);
         String str="Thymeleaf是Web和独立环境的现代服务器端Java模板引擎，能够处理HTML，XML，JavaScript，CSS甚至纯文本。\r\n" +
-                "Thymeleaf的主要目标是提供一种优雅和高度可维护的创建模板的方式。为了实现这一点，它建立在自然模板的概念上，将其逻辑注入到模板文件中，不会影响模板被用作设计原型。这改善了设计的沟通，弥补了设计和开发团队之间的差距。\r\n" +
+                "Thymeleaf的主要目标是提供一种优雅和hhh高度可维护的创建模板的方式。为了实现这一点，它建立在自然模板的概念上，将其逻辑注入到模板文件中，不会影响模板被用作设计原型。这改善了设计的沟通，弥补了设计和开发团队之间的差距。\r\n" +
                 "Thymeleaf也从一开始就设计了Web标准 - 特别是HTML5 - 允许您创建完全验证的模板，如果这是您需要的\r\n";
         model.addAttribute("str",str);
         return "index5";
